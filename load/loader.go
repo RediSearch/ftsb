9ed6f6ddf6ff398cc6dd6d3a8308716c66b6aa4a
filipecosta90 <filipecosta.90@gmail.com>
2019-07-20 16:53:12 +0100

    [fork] forked from tsbs. addapting to full text search use case

---
+++ load/loader.go
@@ -0,0 +1,295 @@
+package load
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	// defaultBatchSize - default size of batches to be inserted
+	defaultBatchSize = 10000
+	defaultReadSize  = 4 << 20 // 4 MB
+
+	// WorkerPerQueue is the value for assigning each worker its own queue of batches
+	WorkerPerQueue = 0
+	// SingleQueue is the value for using a single shared queue across all workers
+	SingleQueue = 1
+
+	errDBExistsFmt = "database \"%s\" exists: aborting."
+)
+
+// change for more useful testing
+var (
+	printFn = fmt.Printf
+	fatal   = log.Fatalf
+)
+
+// Benchmark is an interface that represents the skeleton of a program
+// needed to run an insert or load benchmark.
+type Benchmark interface {
+	// GetPointDecoder returns the PointDecoder to use for this Benchmark
+	GetPointDecoder(br *bufio.Reader) PointDecoder
+
+	// GetBatchFactory returns the BatchFactory to use for this Benchmark
+	GetBatchFactory() BatchFactory
+
+	// GetPointIndexer returns the PointIndexer to use for this Benchmark
+	GetPointIndexer(maxPartitions uint) PointIndexer
+
+	// GetProcessor returns the Processor to use for this Benchmark
+	GetProcessor() Processor
+
+	// GetDBCreator returns the DBCreator to use for this Benchmark
+	GetDBCreator() DBCreator
+}
+
+// BenchmarkRunner is responsible for initializing and storing common
+// flags across all database systems and ultimately running a supplied Benchmark
+type BenchmarkRunner struct {
+	// flag fields
+	dbName          string
+	batchSize       uint
+	workers         uint
+	limit           uint64
+	doLoad          bool
+	doCreateDB      bool
+	doAbortOnExist  bool
+	reportingPeriod time.Duration
+	fileName        string
+
+	// non-flag fields
+	br        *bufio.Reader
+	metricCnt uint64
+	rowCnt    uint64
+}
+
+var loader = &BenchmarkRunner{}
+
+// GetBenchmarkRunner returns the singleton BenchmarkRunner for use in a benchmark program
+// with a default batch size
+func GetBenchmarkRunner() *BenchmarkRunner {
+	return GetBenchmarkRunnerWithBatchSize(defaultBatchSize)
+}
+
+// GetBenchmarkRunnerWithBatchSize returns the singleton BenchmarkRunner for use in a benchmark program
+// with specified batch size.
+func GetBenchmarkRunnerWithBatchSize(batchSize uint) *BenchmarkRunner {
+	// fill flag fields of BenchmarkRunner struct
+	flag.StringVar(&loader.dbName, "db-name", "benchmark", "Name of database")
+	flag.UintVar(&loader.batchSize, "batch-size", batchSize, "Number of items to batch together in a single insert")
+	flag.UintVar(&loader.workers, "workers", 1, "Number of parallel clients inserting")
+	flag.Uint64Var(&loader.limit, "limit", 0, "Number of items to insert (0 = all of them).")
+	flag.BoolVar(&loader.doLoad, "do-load", true, "Whether to write data. Set this flag to false to check input read speed.")
+	flag.BoolVar(&loader.doCreateDB, "do-create-db", true, "Whether to create the database. Disable on all but one client if running on a multi client setup.")
+	flag.BoolVar(&loader.doAbortOnExist, "do-abort-on-exist", false, "Whether to abort if a database with the given name already exists.")
+	flag.DurationVar(&loader.reportingPeriod, "reporting-period", 10*time.Second, "Period to report write stats")
+	flag.StringVar(&loader.fileName, "file", "", "File name to read data from")
+
+	return loader
+}
+
+// DatabaseName returns the value of the --db-name flag (name of the database to store data)
+func (l *BenchmarkRunner) DatabaseName() string {
+	return l.dbName
+}
+
+// RunBenchmark takes in a Benchmark b, a bufio.Reader br, and holders for number of metrics and rows
+// and uses those to run the load benchmark
+func (l *BenchmarkRunner) RunBenchmark(b Benchmark, workQueues uint) {
+	l.br = l.GetBufferedReader()
+
+	// Create required DB
+	cleanupFn := l.useDBCreator(b.GetDBCreator())
+	defer cleanupFn()
+
+	channels := l.createChannels(workQueues)
+
+	// Launch all worker processes in background
+	var wg sync.WaitGroup
+	for i := 0; i < int(l.workers); i++ {
+		wg.Add(1)
+		go l.work(b, &wg, channels[i%len(channels)], i)
+	}
+
+	// Start scan process - actual data read process
+	start := time.Now()
+	l.scan(b, channels)
+
+	// After scan process completed (no more data to come) - begin shutdown process
+
+	// Close all communication channels to/from workers
+	for _, c := range channels {
+		c.close()
+	}
+
+	// Wait for all workers to finish
+	wg.Wait()
+	end := time.Now()
+
+	l.summary(end.Sub(start))
+}
+
+// GetBufferedReader returns the buffered Reader that should be used by the loader
+func (l *BenchmarkRunner) GetBufferedReader() *bufio.Reader {
+	if l.br == nil {
+		if len(l.fileName) > 0 {
+			// Read from specified file
+			file, err := os.Open(l.fileName)
+			if err != nil {
+				fatal("cannot open file for read %s: %v", l.fileName, err)
+				return nil
+			}
+			l.br = bufio.NewReaderSize(file, defaultReadSize)
+		} else {
+			// Read from STDIN
+			l.br = bufio.NewReaderSize(os.Stdin, defaultReadSize)
+		}
+	}
+	return l.br
+}
+
+// useDBCreator handles a DBCreator by running it according to flags set by the
+// user. The function returns a function that the caller should defer or run
+// when the benchmark is finished
+func (l *BenchmarkRunner) useDBCreator(dbc DBCreator) func() {
+	// Empty function to 'defer' from caller
+	closeFn := func() {}
+
+	if l.doLoad {
+		// DBCreator should still be Init'd even if -do-create-db is false since
+		// it can initialize the connecting session
+		dbc.Init()
+
+		switch dbcc := dbc.(type) {
+		case DBCreatorCloser:
+			closeFn = dbcc.Close
+		}
+
+		// Check whether required DB already exists
+		exists := dbc.DBExists(l.dbName)
+		if exists && l.doAbortOnExist {
+			panic(fmt.Sprintf(errDBExistsFmt, l.dbName))
+		}
+
+		// Create required DB if need be
+		// In case DB already exists - delete it
+		if l.doCreateDB {
+			if exists {
+				err := dbc.RemoveOldDB(l.dbName)
+				if err != nil {
+					panic(err)
+				}
+			}
+			err := dbc.CreateDB(l.dbName)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		switch dbcp := dbc.(type) {
+		case DBCreatorPost:
+			dbcp.PostCreateDB(l.dbName)
+		}
+	}
+	return closeFn
+}
+
+// createChannels create channels from which workers would receive tasks
+// Number of workers may be different from number of channels, thus we may have
+// multiple workers per channel
+func (l *BenchmarkRunner) createChannels(workQueues uint) []*duplexChannel {
+	// Result - channels to be created
+	channels := []*duplexChannel{}
+
+	// How many work queues should be created?
+	workQueuesToCreate := workQueues
+	if workQueues == WorkerPerQueue {
+		workQueuesToCreate = l.workers
+	} else if workQueues > l.workers {
+		panic(fmt.Sprintf("cannot have more work queues (%d) than workers (%d)", workQueues, l.workers))
+	}
+
+	// How many workers would be served by each queue?
+	workersPerQueue := int(math.Ceil(float64(l.workers) / float64(workQueuesToCreate)))
+
+	// Create duplex communication channels
+	for i := uint(0); i < workQueuesToCreate; i++ {
+		channels = append(channels, newDuplexChannel(workersPerQueue))
+	}
+
+	return channels
+}
+
+// scan launches any needed reporting mechanism and proceeds to scan input data
+// to distribute to workers
+func (l *BenchmarkRunner) scan(b Benchmark, channels []*duplexChannel) uint64 {
+	// Start background reporting process
+	// TODO why it is here? May be it could be moved one level up?
+	if l.reportingPeriod.Nanoseconds() > 0 {
+		go l.report(l.reportingPeriod)
+	}
+
+	// Scan incoming data
+	return scanWithIndexer(channels, l.batchSize, l.limit, l.br, b.GetPointDecoder(l.br), b.GetBatchFactory(), b.GetPointIndexer(uint(len(channels))))
+}
+
+// work is the processing function for each worker in the loader
+func (l *BenchmarkRunner) work(b Benchmark, wg *sync.WaitGroup, c *duplexChannel, workerNum int) {
+
+	// Prepare processor
+	proc := b.GetProcessor()
+	proc.Init(workerNum, l.doLoad)
+
+	// Process batches coming from duplexChannel.toWorker queue
+	// and send ACKs into duplexChannel.toScanner queue
+	for b := range c.toWorker {
+		metricCnt, rowCnt := proc.ProcessBatch(b, l.doLoad)
+		atomic.AddUint64(&l.metricCnt, metricCnt)
+		atomic.AddUint64(&l.rowCnt, rowCnt)
+		c.sendToScanner()
+	}
+
+	// Close proc if necessary
+	switch c := proc.(type) {
+	case ProcessorCloser:
+		c.Close(l.doLoad)
+	}
+
+	wg.Done()
+}
+
+// summary prints the summary of statistics from loading
+func (l *BenchmarkRunner) summary(took time.Duration) {
+	metricRate := float64(l.metricCnt) / float64(took.Seconds())
+	printFn("\nSummary:\n")
+	printFn("loaded %d Documents in %0.3fsec with %d workers (mean rate %0.2f metrics/sec)\n", l.metricCnt, took.Seconds(), l.workers, metricRate)
+}
+
+// report handles periodic reporting of loading stats
+func (l *BenchmarkRunner) report(period time.Duration) {
+	start := time.Now()
+	prevTime := start
+	prevDocumentCount := uint64(0)
+
+	printFn("time,per. docs/s,docs total,overall docs/s\n")
+	for now := range time.NewTicker(period).C {
+		documentCount := atomic.LoadUint64(&l.metricCnt)
+
+		sinceStart := now.Sub(start)
+		took := now.Sub(prevTime)
+		colrate := float64(documentCount-prevDocumentCount) / float64(took.Seconds())
+		overallColRate := float64(documentCount) / float64(sinceStart.Seconds())
+
+			printFn("%d,%0.2f,%E,%0.2f\n", now.Unix(), colrate, float64(documentCount), overallColRate)
+
+		prevDocumentCount = documentCount
+		prevTime = now
+	}
+}
---
+++ load/loader_test.go
@@ -0,0 +1,513 @@
+package load
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+type testProcessor struct {
+	worker int
+	closed bool
+}
+
+func (p *testProcessor) Init(workerNum int, _ bool) {
+	p.worker = workerNum
+}
+
+func (p *testProcessor) ProcessBatch(b Batch, doLoad bool) (metricCount, rowCount uint64) {
+	return 1, 0
+}
+
+func (p *testProcessor) Close(_ bool) {
+	p.closed = true
+}
+
+type testCreator struct {
+	exists    bool
+	errRemove bool
+	errCreate bool
+
+	initCalled   bool
+	createCalled bool
+	removeCalled bool
+	postCalled   bool
+	closedCalled bool
+}
+
+func (c *testCreator) Init() {
+	c.initCalled = true
+}
+func (c *testCreator) DBExists(dbName string) bool {
+	return c.exists
+}
+func (c *testCreator) CreateDB(dbName string) error {
+	c.createCalled = true
+	if c.errCreate {
+		return fmt.Errorf("create error")
+	}
+	return nil
+}
+func (c *testCreator) RemoveOldDB(dbName string) error {
+	c.removeCalled = true
+	if c.errRemove {
+		return fmt.Errorf("remove error")
+	}
+	return nil
+}
+
+type testCreatorPost struct {
+	testCreator
+}
+
+func (c *testCreatorPost) PostCreateDB(dbName string) error {
+	c.postCalled = true
+	return nil
+}
+
+type testCreatorClose struct {
+	testCreator
+}
+
+func (c *testCreatorClose) Close() {
+	c.closedCalled = true
+}
+
+type testBenchmark struct {
+	processors []*testProcessor
+	offset     int64
+}
+
+func (b *testBenchmark) GetPointDecoder(_ *bufio.Reader) PointDecoder    { return nil }
+func (b *testBenchmark) GetBatchFactory() BatchFactory                   { return nil }
+func (b *testBenchmark) GetPointIndexer(maxPartitions uint) PointIndexer { return &ConstantIndexer{} }
+func (b *testBenchmark) GetProcessor() Processor {
+	idx := atomic.AddInt64(&b.offset, 1)
+	idx--
+	return b.processors[idx]
+}
+func (b *testBenchmark) GetDBCreator() DBCreator {
+	return nil
+}
+
+func TestGetBufferedReader(t *testing.T) {
+	r := &BenchmarkRunner{}
+	br := r.br
+	if br != nil {
+		t.Errorf("initial buffered reader is non-nil")
+	}
+
+	oldFatal := fatal
+	fatalCalled := false
+	fatal = func(format string, args ...interface{}) {
+		fatalCalled = true
+	}
+
+	// Should give a nil bufio.Reader
+	fatalCalled = false
+	r.fileName = "foo"
+	br = r.GetBufferedReader()
+	if br != nil {
+		t.Errorf("filename returned not nil buffered reader for unexistent file")
+	}
+
+	if !fatalCalled {
+		t.Errorf("fatal not called when it should have been")
+	}
+
+	// Should give a non-nil bufio.Reader
+	fatalCalled = false
+	r.fileName = "/dev/null"
+	br = r.GetBufferedReader()
+	if br == nil {
+		t.Errorf("filename returned nil buffered reader for /dev/null")
+	}
+
+	// Should give a non-nil bufio.Reader
+	fatalCalled = false
+	r.fileName = ""
+	br = r.GetBufferedReader()
+	if br == nil {
+		t.Errorf("STDOUT returned a nil buffered reader")
+	}
+
+	// Test that it returns same bufio.Reader as before
+	fatalCalled = false
+	old := br
+	br = r.GetBufferedReader()
+	if br != old {
+		t.Errorf("different buffered reader returned after previously set")
+	}
+
+	fatal = oldFatal
+}
+
+func TestUseDBCreator(t *testing.T) {
+	cases := []struct {
+		desc         string
+		doLoad       bool
+		exists       bool
+		abortOnExist bool
+		doCreate     bool
+		doPost       bool
+		doClose      bool
+
+		shouldPanic bool
+		errRemove   bool
+		errCreate   bool
+	}{
+		{
+			desc:   "doLoad is false",
+			doLoad: false,
+		},
+		{
+			desc:         "doLoad is true, nothing else",
+			doLoad:       true,
+			exists:       false,
+			abortOnExist: false,
+			doCreate:     false,
+		},
+		{
+			desc:     "doLoad, doCreate = true",
+			doLoad:   true,
+			doCreate: true,
+		},
+		{
+			desc:     "doLoad, doCreate, exists = true",
+			doLoad:   true,
+			doCreate: true,
+			exists:   true,
+		},
+		{
+			desc:     "post create = true",
+			doLoad:   true,
+			exists:   false,
+			doCreate: true,
+			doPost:   true,
+		},
+		{
+			desc:    "close = true",
+			doLoad:  true,
+			exists:  false,
+			doClose: true,
+		},
+		{
+			desc:         "exists, doAbortOnExist = true, should panic",
+			doLoad:       true,
+			exists:       true,
+			abortOnExist: true,
+			shouldPanic:  true,
+		},
+		{
+			desc:        "removeDB errs, should panic",
+			doLoad:      true,
+			doCreate:    true,
+			exists:      true,
+			errRemove:   true,
+			shouldPanic: true,
+		},
+
+		{
+			desc:        "createDB errs, should panic",
+			doLoad:      true,
+			doCreate:    true,
+			exists:      true,
+			errCreate:   true,
+			shouldPanic: true,
+		},
+	}
+	testPanic := func(r *BenchmarkRunner, dbc DBCreator, desc string) {
+		defer func() {
+			if re := recover(); re == nil {
+				t.Errorf("%s: did not panic when should", desc)
+			}
+		}()
+		_ = r.useDBCreator(dbc)
+	}
+	for _, c := range cases {
+		r := &BenchmarkRunner{
+			doLoad:         c.doLoad,
+			doCreateDB:     c.doCreate,
+			doAbortOnExist: c.abortOnExist,
+		}
+		core := testCreator{
+			exists:    c.exists,
+			errCreate: c.errCreate,
+			errRemove: c.errRemove,
+		}
+
+		// Decide whether to decorate the core DBCreator
+		var dbc DBCreator
+		if c.doPost {
+			dbc = &testCreatorPost{core}
+		} else if c.doClose {
+			dbc = &testCreatorClose{core}
+		} else {
+			dbc = &core
+		}
+
+		if c.shouldPanic {
+			testPanic(r, dbc, c.desc)
+			continue
+		}
+
+		deferFn := r.useDBCreator(dbc)
+		deferFn()
+
+		// Recover the core if decorated
+		if c.doPost {
+			core = dbc.(*testCreatorPost).testCreator
+		} else if c.doClose {
+			core = dbc.(*testCreatorClose).testCreator
+		}
+		if c.doLoad {
+			if !core.initCalled {
+				t.Errorf("%s: doLoad is true but Init not called", c.desc)
+			}
+			if c.doCreate {
+				if !core.createCalled {
+					t.Errorf("%s: doCreate is true but CreateDB not called", c.desc)
+				}
+				if c.exists {
+					if !core.removeCalled {
+						t.Errorf("%s: exists is true but RemoveDB not called", c.desc)
+					}
+				} else if core.removeCalled {
+					t.Errorf("%s: exists is false but RemoveDB was called", c.desc)
+				}
+			} else if core.createCalled {
+				t.Errorf("%s: doCreate is false but CreateDB was called", c.desc)
+			}
+			if c.doPost && !core.postCalled {
+				t.Errorf("%s: doPost is true but PostCreateDB not called", c.desc)
+			} else if !c.doPost && core.postCalled {
+				t.Errorf("%s: doPost is false but PostCreateDB was called", c.desc)
+			}
+		} else if core.initCalled {
+			t.Errorf("%s: doLoad is false but Init not called", c.desc)
+		}
+
+		// Test closing function is set or not set
+		if c.doClose != core.closedCalled {
+			t.Errorf("%s: close condition not equal: got %v want %v", c.desc, core.closedCalled, c.doClose)
+		}
+	}
+}
+
+func TestCreateChannelsAndPartitions(t *testing.T) {
+	cases := []struct {
+		desc           string
+		queues         uint
+		workers        uint
+		wantPartitions uint
+		wantChanLen    int
+		shouldPanic    bool
+	}{
+		{
+			desc:           "single queue",
+			queues:         SingleQueue,
+			workers:        2,
+			wantPartitions: 1,
+			wantChanLen:    2,
+		},
+		{
+			desc:           "worker per queue",
+			queues:         WorkerPerQueue,
+			workers:        2,
+			wantPartitions: 2,
+			wantChanLen:    1,
+		},
+		{
+			desc:           "workers divide evenly into queues",
+			queues:         3,
+			workers:        6,
+			wantPartitions: 3,
+			wantChanLen:    2,
+		},
+		{
+			desc:           "workers do not divide evenly into queues",
+			queues:         3,
+			workers:        7,
+			wantPartitions: 3,
+			wantChanLen:    3,
+		},
+		{
+			desc:           "too many queues for workers, panic",
+			queues:         3,
+			workers:        2,
+			wantPartitions: 0,
+			wantChanLen:    0,
+			shouldPanic:    true,
+		},
+	}
+	testPanic := func(br *BenchmarkRunner, queues uint, desc string) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("%s: did not panic when should", desc)
+			}
+		}()
+		_ = br.createChannels(queues)
+	}
+	for _, c := range cases {
+		br := &BenchmarkRunner{}
+		br.workers = c.workers
+		if c.shouldPanic {
+			testPanic(br, c.queues, c.desc)
+		} else {
+			channels := br.createChannels(c.queues)
+			if got := uint(len(channels)); got != c.wantPartitions {
+				t.Errorf("%s: incorrect number of partitions: got %d want %d", c.desc, got, c.wantPartitions)
+			}
+			if got := cap(channels[0].toWorker); got != c.wantChanLen {
+				t.Errorf("%s: incorrect channel length: got %d want %d", c.desc, got, c.wantChanLen)
+			}
+		}
+	}
+}
+
+func TestWork(t *testing.T) {
+	br := loader
+	b := &testBenchmark{}
+	for i := 0; i < 2; i++ {
+		b.processors = append(b.processors, &testProcessor{})
+	}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	c := newDuplexChannel(2)
+	c.sendToWorker(&testBatch{})
+	c.sendToWorker(&testBatch{})
+	go br.work(b, &wg, c, 0)
+	time.Sleep(100 * time.Millisecond)
+	go br.work(b, &wg, c, 1)
+	<-c.toScanner
+	<-c.toScanner
+	c.close()
+	wg.Wait()
+
+	if got := b.processors[0].worker; got != 0 {
+		t.Errorf("TestWork: processor 0 has wrong worker id: got %d want %d", got, 0)
+	}
+
+	if got := b.processors[1].worker; got != 1 {
+		t.Errorf("TestWork: processor 0 has wrong worker id: got %d want %d", got, 1)
+	}
+
+	if got := br.metricCnt; got != 2 {
+		t.Errorf("TestWork: invalid metric count: got %d want %d", got, 2)
+	}
+
+	if !b.processors[0].closed {
+		t.Errorf("TestWork: processor 0 not closed")
+	}
+
+	if !b.processors[1].closed {
+		t.Errorf("TestWork: processor 1 not closed")
+	}
+}
+
+func TestSummary(t *testing.T) {
+	cases := []struct {
+		desc    string
+		metrics uint64
+		rows    uint64
+		took    time.Duration
+		want    string
+	}{
+		{
+			desc:    "10 metrics, 0 rows, 1 second",
+			metrics: 10,
+			rows:    0,
+			took:    time.Second,
+			want:    "\nSummary:\nloaded 10 metrics in 1.000sec with 0 workers (mean rate 10.00 metrics/sec)\n",
+		},
+		{
+			desc:    "fractional rate: 10 metrics, 0 rows, 1 second",
+			metrics: 15,
+			rows:    0,
+			took:    10 * time.Second,
+			want:    "\nSummary:\nloaded 15 metrics in 10.000sec with 0 workers (mean rate 1.50 metrics/sec)\n",
+		},
+		{
+			desc:    "fraction time: 10 metrics, 0 rows, .5 second",
+			metrics: 10,
+			rows:    0,
+			took:    500 * time.Millisecond,
+			want:    "\nSummary:\nloaded 10 metrics in 0.500sec with 0 workers (mean rate 20.00 metrics/sec)\n",
+		},
+		{
+			desc:    "include rows: 10 metrics, 1 rows, 1 second",
+			metrics: 10,
+			rows:    1,
+			took:    time.Second,
+			want:    "\nSummary:\nloaded 10 metrics in 1.000sec with 0 workers (mean rate 10.00 metrics/sec)\nloaded 1 rows in 1.000sec with 0 workers (mean rate 1.00 rows/sec)\n",
+		},
+	}
+
+	for _, c := range cases {
+		br := &BenchmarkRunner{}
+		br.metricCnt = c.metrics
+		br.rowCnt = c.rows
+		var b bytes.Buffer
+		printFn = func(s string, args ...interface{}) (n int, err error) {
+			return fmt.Fprintf(&b, s, args...)
+		}
+		br.summary(c.took)
+		if got := string(b.Bytes()); got != c.want {
+			t.Errorf("%s: incorrect summary\ngot %s\nwant %s", c.desc, got, c.want)
+		}
+	}
+}
+
+func TestReport(t *testing.T) {
+	var b bytes.Buffer
+	counter := int64(0)
+	var m sync.Mutex
+	printFn = func(s string, args ...interface{}) (n int, err error) {
+		atomic.AddInt64(&counter, 1)
+		m.Lock()
+		defer m.Unlock()
+		return fmt.Fprintf(&b, s, args...)
+	}
+	br := &BenchmarkRunner{}
+	duration := 200 * time.Millisecond
+	go br.report(duration)
+
+	time.Sleep(25 * time.Millisecond)
+	if got := atomic.LoadInt64(&counter); got != 1 {
+		t.Errorf("TestReport: header count check incorrect: got %d want %d", got, 1)
+	}
+
+	time.Sleep(duration)
+	if got := atomic.LoadInt64(&counter); got != 2 {
+		t.Errorf("TestReport: counter check incorrect (1): got %d want %d", got, 2)
+	}
+
+	time.Sleep(duration)
+	if got := atomic.LoadInt64(&counter); got != 3 {
+		t.Errorf("TestReport: counter check incorrect (2): got %d want %d", got, 3)
+	}
+	m.Lock()
+	end := strings.TrimSpace(string(b.Bytes()))
+	m.Unlock()
+	if end[len(end)-1:len(end)] != "-" {
+		t.Errorf("TestReport: non-row report does not end in -")
+	}
+
+	// update row count so line is different
+	atomic.StoreUint64(&br.rowCnt, 1)
+	time.Sleep(duration)
+	if got := atomic.LoadInt64(&counter); got != 4 {
+		t.Errorf("TestReport: counter check incorrect (1): got %d want %d", got, 4)
+	}
+	m.Lock()
+	end = strings.TrimSpace(string(b.Bytes()))
+	m.Unlock()
+	if end[len(end)-1:len(end)] == "-" {
+		t.Errorf("TestReport: row report ends in -")
+	}
+}
