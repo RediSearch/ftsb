d48b5ba4e18f0417a8d8829843de4ec5f030a439
filipecosta90 <filipecosta.90@gmail.com>
2019-10-15 16:15:37 +0100

    [add] improving helper scripts

--- scripts/run_ftsb_redisearch_pages.sh
+++ scripts/run_ftsb_redisearch_pages.sh
@@ -32,7 +32,7 @@ WORKERS=${WORKERS:-$(grep -c ^processor /proc/cpuinfo 2>/dev/null || echo 8)}
 echo "Benchmarking query execution performance"
 echo "Using ${WORKERS} WORKERS"
 #for queryName in  "agg-0-*" "agg-1-editor-1year-exact-page-contributions-by-day" "agg-2-*-1month-exact-distinct-editors-by-hour" "agg-3-*-1month-approximate-distinct-editors-by-hour" "agg-4-*-1day-approximate-page-contributions-by-5minutes-by-editor-username" "agg-5-*-1month-approximate-top10-editor-usernames" "agg-6-*-1month-approximate-top10-editor-usernames-by-namespace" "agg-7-*-1month-avg-revision-content-length-by-editor-username" "agg-8-editor-approximate-avg-editor-contributions-by-year"; do
-for queryName in "agg-0-*" "agg-1-editor-1year-exact-page-contributions-by-day" "agg-2-*-1month-exact-distinct-editors-by-hour" "agg-3-*-1month-approximate-distinct-editors-by-hour" "agg-4-*-1day-approximate-page-contributions-by-5minutes-by-editor-username" "agg-5-*-1month-approximate-top10-editor-usernames" "agg-6-*-1month-approximate-top10-editor-usernames-by-namespace" "agg-7-*-1month-avg-revision-content-length-by-editor-username" "agg-8-editor-approximate-avg-editor-contributions-by-year"; do
+for queryName in "agg-1-editor-1year-exact-page-contributions-by-day" "agg-2-*-1month-exact-distinct-editors-by-hour" "agg-3-*-1month-approximate-distinct-editors-by-hour" "agg-4-*-1day-approximate-page-contributions-by-5minutes-by-editor-username" "agg-5-*-1month-approximate-top10-editor-usernames" "agg-6-*-1month-approximate-top10-editor-usernames-by-namespace" "agg-7-*-1month-avg-revision-content-length-by-editor-username" "agg-8-editor-approximate-avg-editor-contributions-by-year"; do
   echo "Benchmarking query: $queryName"
 
   if [ -f /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.gz ]; then
@@ -54,13 +54,11 @@ for queryName in "agg-0-*" "agg-1-editor-1year-exact-page-contributions-by-day" 
 
   sleep ${SLEEP_BETWEEN_RUNS}
 
-
 done
-
 echo "Benchmarking WITH_CURSOR query execution performance"
 
 MAX_QUERIES=1000
-WITH_CURSOR=false
+WITH_CURSOR=true
 
 for queryName in "agg-0-*"; do
   echo "Benchmarking query: $queryName"
