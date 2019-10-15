e242af8aa27be126007eddcd30e156baf1bd2590
filipecosta90 <filipecosta.90@gmail.com>
2019-10-15 15:53:48 +0100

    [add] improving helper scripts

--- scripts/run_ftsb_redisearch_pages.sh
+++ scripts/run_ftsb_redisearch_pages.sh
@@ -32,9 +32,8 @@ WORKERS=${WORKERS:-$(grep -c ^processor /proc/cpuinfo 2>/dev/null || echo 8)}
 echo "Benchmarking query execution performance"
 echo "Using ${WORKERS} WORKERS"
 #for queryName in  "agg-0-*" "agg-1-editor-1year-exact-page-contributions-by-day" "agg-2-*-1month-exact-distinct-editors-by-hour" "agg-3-*-1month-approximate-distinct-editors-by-hour" "agg-4-*-1day-approximate-page-contributions-by-5minutes-by-editor-username" "agg-5-*-1month-approximate-top10-editor-usernames" "agg-6-*-1month-approximate-top10-editor-usernames-by-namespace" "agg-7-*-1month-avg-revision-content-length-by-editor-username" "agg-8-editor-approximate-avg-editor-contributions-by-year"; do
-for queryName in "agg-0-*"; do
+for queryName in "agg-0-*" "agg-1-editor-1year-exact-page-contributions-by-day" "agg-2-*-1month-exact-distinct-editors-by-hour" "agg-3-*-1month-approximate-distinct-editors-by-hour" "agg-4-*-1day-approximate-page-contributions-by-5minutes-by-editor-username" "agg-5-*-1month-approximate-top10-editor-usernames" "agg-6-*-1month-approximate-top10-editor-usernames-by-namespace" "agg-7-*-1month-avg-revision-content-length-by-editor-username" "agg-8-editor-approximate-avg-editor-contributions-by-year"; do
   echo "Benchmarking query: $queryName"
-  #redis-cli -h $IP -p $PORT config resetstat
 
   if [ -f /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.gz ]; then
     cat /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.gz |
@@ -51,10 +50,33 @@ for queryName in "agg-0-*"; do
   fi
 
   echo "Query $queryName Redis Command Statistics"
-  # redis-cli -h $IP -p $PORT info commandstats 2>&1 | tee ~/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0_commandstats.txt
+  redis-cli -h $IP -p $PORT ft.info $IDX >~/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0_ft.info.txt
 
-  #redis-cli -h $IP -p $PORT ft.info $IDX >~/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0_ft.info.txt
+  sleep ${SLEEP_BETWEEN_RUNS}
 
-  #sleep $SLEEP_BETWEEN_RUNS
 
 done
+
+echo "Benchmarking WITH_CURSOR query execution performance"
+
+MAX_QUERIES=1000
+WITH_CURSOR=false
+
+for queryName in "agg-0-*"; do
+  echo "Benchmarking query: $queryName"
+
+  if [ -f /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.gz ]; then
+    cat /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.gz |
+      gunzip >/tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0
+
+    ftsb_run_queries_redisearch \
+      -file /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0 \
+      -index=${IDX} \
+      -host=${HOST} \
+      -max-queries ${MAX_QUERIES} -with-cursor=${WITH_CURSOR} \
+      -workers ${WORKERS} -print-interval ${PRINT_INTERVAL} 2>&1 | tee ~/WITH_CURSOR-redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.txt
+  else
+    echo "query file for $queryName not found at /tmp/redisearch-queries-$DATASET-$queryName-100K-queries-1-0-0.gz"
+  fi
+done
+
