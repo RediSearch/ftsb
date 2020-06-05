5510f439a19b5c02a4d3c8acab1730c9448862c4
filipecosta90 <filipecosta.90@gmail.com>
2020-06-05 12:15:52 +0100

    [fix] removing scripts bloat. (part of simplification process)

--- scripts/benchmark/synthetic_numeric.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Load parameters - common
-EXE_DIR=${EXE_DIR:-$(dirname $0)}
-source ${EXE_DIR}/../common_vars.sh
-source ${EXE_DIR}/../usecases/synthetic_numeric.sh
-
-# Run the loader
-source ${EXE_DIR}/load.sh
--- scripts/usecases/synthetic_text.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-USE_CASE="synthetic-text"
-DATAFILE_RAW=/tmp/$USE_CASE.xml
-DATAFILE_LOADER=/tmp/$USE_CASE.ftsb
-
-# Index to benchmark the databuild into
-IDX=${IDX:-"synthetic-text-idx1"}
